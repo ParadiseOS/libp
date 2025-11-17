c2a5e94dae88fcee7a70a8820ea7a3c5f5267859
Tripplenut <afabius05@gmail.com>
2025-11-17 17:28:39 -0500

    Updates libp for paradise ipc changes
    With the redesign of the ParadiseIPC we need to alter the syscalls for
    sending and receiving messages. Also needed to add the structs
    associated with messages.

--- src/pio.s
+++ src/pio.s
@@ -2,21 +2,19 @@ format ELF
 
 section ".text" executable
 
-; Syscall as of  9/19/25
+; Syscall as of  11/12/25
 
 public write1
 write1:
-
-    mov eax, 1          ; print string w/ length syscall
+    mov eax, 99         ; print string w/ length syscall
     mov ebx, [esp + 4]  ; pointer to string
     mov ecx, [esp + 8]  ; size of string
-
     int 0x80
     ret
 
 public write2
 write2:
-    mov eax, 2          ; print string syscall
+    mov eax, 100        ; print string syscall
     mov ebx, [esp + 4]  ; str
     int 0x80
     ret
--- src/pmail.s
+++ src/pmail.s
@@ -2,19 +2,24 @@ format ELF
 
 section ".text" executable
 
+; Syscall as of  11/12/25
+
 public send_message
 send_message:
-    mov eax, 3          ; send message syscall
-    mov ebx, [esp + 4]  ; receiver_pid
-    mov ecx, [esp + 8]  ; size
+    mov eax, 0          ; send message syscall
+    mov ebx, [esp + 4]  ; reader pid
+    mov ecx, [esp + 8]  ; data_size
     mov edx, [esp + 12] ; data pointer
+    mov esi, [esp + 16] ; flags
     int 0x80
-    ret
+    ret ; return status in eax
 
 public read_message
 read_message:
-    mov eax, 4           ; read_message syscall
-    mov ebx, [esp + 4]   ; pointer to message buffer
+    mov eax, 1           ; read_message syscall
+    mov ebx, [esp + 4]   ; sender pid
+    mov ecx, [esp + 8]   ; reader pid
+    mov edx, [esp + 12]  ; mailbox message ptr
+    mov esi, [esp + 16]  ; flags
     int 0x80
-    mov eax, ecx ; return status
-    ret
+    ret ; return status in eax
